26c0b1944d82c4f4c7ca33e0335563d3ffa69fd0
omerdor001 <omerdo@post.bgu.ac.il>
2025-01-18 19:41:53 +0200

    Provengo behavior.js

--- Provengo/helloprovengo/spec/js/behavior.js
+++ Provengo/helloprovengo/spec/js/behavior.js
@@ -3,37 +3,37 @@
 bthread('Reply', function () {
   let session = new SeleniumSession('reply');
   session.start(URL, 'chrome');
+  loginAdmin(session, moodledata);
+  createCourse(session,moodledata);
 
-  // Log in as a student
+  bp.sync({ request: bp.Event("navigateToCourse") });
+    navigateToCourseFromHomePage(session);
+
+  enrollStudent(session);
+  createForum(session,moodledata);
+
+  logout(session);
   loginStudent(session, moodledata);
 
-  // Wait for and navigate to the course
-  bp.sync({ request: bp.Event("navigateToCourse") });
-  navigateToCourseFromHomePage(session);
-
-  // Wait for and navigate to the forum
   bp.sync({ request: bp.Event("navigateToForum") });
   navigateToForum(session);
 
-  // Wait for and navigate to the topic
+  createTopic(session,moodledata);
+
   bp.sync({ request: bp.Event("navigateToTopic") });
   navigateToTopic(session);
 
-  // Block hiding the forum during this interaction
   bp.sync({ block: bp.Event("hideForum") });
 
-  // Wait for and add a comment to the forum
   bp.sync({ request: bp.Event("commentOnForum") });
   commentOnForum(session, moodledata);
 
-  // Check if the comment exists
   if (checkCommentExist(session)) {
     console.log('Comment successfully added.');
   } else {
     console.error('Comment could not be added.');
   }
 
-  // Wait for and log out
   bp.sync({ request: bp.Event("logout") });
   logout(session);
 
@@ -42,34 +42,35 @@ bthread('Reply', function () {
 
 bthread('Hide', function () {
   let session = new SeleniumSession('hide');
-  session.start(URL, 'chrome'); // Ensure browser type is explicitly passed
+  session.start(URL, 'chrome');
 
-  // Log in as a teacher
-  loginTeacher(session, moodledata);
+  loginAdmin(session, moodledata);
+  createCourse(session,moodledata);
 
-  // Wait for and navigate to the course
   bp.sync({ request: bp.Event("navigateToCourse") });
   navigateToCourseFromHomePage(session);
 
-  // Wait for and navigate to the forum
+  enrollStudent(session);
+  enrollTeacher(session);
+  createForum(session,moodledata);
+
+  logout(session);
+  loginTeacher(session,moodledata);
+
   bp.sync({ request: bp.Event("navigateToForum") });
   navigateToForum(session);
 
-  // Block commenting on the forum during this interaction
   bp.sync({ block: bp.Event("commentOnForum") });
 
-  // Wait for and hide the forum
   bp.sync({ request: bp.Event("hideForum") });
   hideForum(session);
 
-  // Check if the forum is successfully hidden
   if (checkForumHiding(session)) {
     console.log('Forum successfully hidden.');
   } else {
     console.error('Forum could not be hidden.');
   }
 
-  // Wait for and log out
   bp.sync({ request: bp.Event("logout") });
   logout(session);
 
