d8e5086e7ddaaa170cefaa6dc40084233edbcab4
omerdor001 <omerdo@post.bgu.ac.il>
2025-01-18 15:14:22 +0200

    Provengo data

--- Provengo/helloprovengo/data/data.js
+++ Provengo/helloprovengo/data/data.js
@@ -35,18 +35,74 @@ const xpaths = {
     enterForumName: "//*[@id=\"id_name\"]",
     createForumButton: "//*[@id=\"id_submitbutton\"]"
   }
-  CreateTopic: {
-
-  }
-
-
-
-  searchWindow: {
-    searchInput: '//textarea[@aria-label="Search"]',
-    searchButton: '//input[@aria-label="Google Search"]',
-    feelingLuckyButton: '//input[@aria-label="I\'m Feeling Lucky"]',
-
-  }
+   CreateTopic: {
+      addNewTopic: "/html/body/div[5]/div[5]/div/div[3]/div/section/div[2]/div[1]/div/div[2]/a",
+      enterTopicSubject: "//[@id="id_subject"]",
+      enterMessageIframe: "//[@id="id_message_ifr"]",
+      enterMessageBody: "tinymce",
+      submitButton: "//[@id="id_submitbutton"]",
+      returnToForum: "/html/body/div[5]/div[5]/div/header/div/div[1]/div[1]/nav/ol/li[2]/a"
+    }
+    NavigateToForum: {
+      forumLink: "/html/body/div[2]/div[4]/div/div[3]/div/section/div/div/div/ul/li[1]/div/div[2]/ul/li[2]/div/div[2]/div[2]/div/div/a"
+    }
+    CommentForum: {
+      replyButton: "/html/body/div[2]/div[4]/div/div[2]/div/section/div[2]/div/article/div[1]/div/div/div[2]/div[2]/div/a[2]",
+      enterReplyTextArea: "/html/body/div[2]/div[4]/div/div[2]/div/section/div[2]/div/article/div[1]/div/div[2]/div/form/div[1]/span/textarea",
+      postReplyButton: "/html/body/div[2]/div[4]/div/div[2]/div/section/div[2]/div/article/div[1]/div/div[2]/div/form/div[2]/button[1]/span[1]"
+    }
+    NavigateToTopic: {
+      forumLink: "/html/body/div[3]/div[4]/div/div[2]/div/section/div[2]/div[2]/div[3]/div/table/tbody/tr/th/div/div[1]/a"
+    }
+    Logout: {
+      userMenuToggle: "//[@id="user-menu-toggle"]",
+      logoutLink: "//*[@id="carousel-item-main"]/a[10]"
+    }
+    LogoutAfterReplying: {
+      userMenuToggle: "//[@id="user-menu-toggle"]",
+      logoutLink: "//*[@id="carousel-item-main"]/a[9]"
+    },
+    CheckCommentExist: {
+      replyExist: "//*[contains(text(),'reply')]"
+    },
+    EnrollTeacher: {
+       navigateToParticipates: "/html/body/div[2]/div[4]/div/div[2]/nav/ul/li[3]/a",
+       enrollUserButton: "//input[@type='submit' and @class='btn btn-primary']",
+       selectUserComboBox: "//input[@role='combobox' and @data-fieldtype='autocomplete']",
+       enrollTeacherRoleDropdown: "//[@id='id_roletoassign']",
+       teacherRoleValue: "3",
+       enrollButton: "//button[@type='button' and @class='btn btn-primary' and @data-action='save']",
+       navigateToCourseHome: "//li[@data-key='coursehome']//a"
+    }
+    HideForum: {
+      editForumButton: "//li[@data-key='modedit']//a",
+      forumVisibilitySection: "//[@id='collapseElement-8']",
+      visibilityDropdown: "//[@id='id_visible']",
+      hideOnCoursePageOption: "Hide on course page",
+      saveChangesButton: "//[@id='id_submitbutton']"
+    }
+    CheckForumHiding: {
+     hiding: "//*[@id=\"section-0\"]"
+    }
 }
 
-const searchTerm = 'pizza'
+const moodledata = {
+  Login: {
+    adminUsername: "admin",
+    adminPassword: "sandbox24",
+    teacherUsername: "teacher",
+    teacherPassword: "sandbox24",
+    studentUsername: "student",
+    studentPassword: "sandbox24"
+  },
+  Course: {
+    fullName: "test_course",
+    shortName: "test"
+  },
+  Forum: {
+    forumName: "test",
+    topicSubject: "test",
+    topicMessage: "test",
+    replyMessage: "reply"
+  }
+};
