335074baaed56a2818c9d88bf81ea396202a2c8f
omerdor001 <omerdo@post.bgu.ac.il>
2025-01-18 19:47:18 +0200

    Provengo behavior.js

--- Provengo/helloprovengo/spec/js/behavior.js
+++ Provengo/helloprovengo/spec/js/behavior.js
@@ -3,22 +3,33 @@
 bthread('Reply', function () {
   let session = new SeleniumSession('reply');
   session.start(URL, 'chrome');
+
+  bp.sync({ request: bp.Event("loginAdmin") });
   loginAdmin(session, moodledata);
-  createCourse(session,moodledata);
+
+  bp.sync({ request: bp.Event("createCourse") });
+  createCourse(session, moodledata);
 
   bp.sync({ request: bp.Event("navigateToCourse") });
-    navigateToCourseFromHomePage(session);
+  navigateToCourseFromHomePage(session);
 
+  bp.sync({ request: bp.Event("enrollStudent") });
   enrollStudent(session);
-  createForum(session,moodledata);
 
+  bp.sync({ request: bp.Event("createForum") });
+  createForum(session, moodledata);
+
+  bp.sync({ request: bp.Event("logoutAdmin") });
   logout(session);
+
+  bp.sync({ request: bp.Event("loginStudent") });
   loginStudent(session, moodledata);
 
   bp.sync({ request: bp.Event("navigateToForum") });
   navigateToForum(session);
 
-  createTopic(session,moodledata);
+  bp.sync({ request: bp.Event("createTopic") });
+  createTopic(session, moodledata);
 
   bp.sync({ request: bp.Event("navigateToTopic") });
   navigateToTopic(session);
@@ -28,13 +39,14 @@ bthread('Reply', function () {
   bp.sync({ request: bp.Event("commentOnForum") });
   commentOnForum(session, moodledata);
 
+  bp.sync({ request: bp.Event("checkCommentExist") });
   if (checkCommentExist(session)) {
     console.log('Comment successfully added.');
   } else {
     console.error('Comment could not be added.');
   }
 
-  bp.sync({ request: bp.Event("logout") });
+  bp.sync({ request: bp.Event("logoutStudent") });
   logout(session);
 
   session.stop();
@@ -44,18 +56,29 @@ bthread('Hide', function () {
   let session = new SeleniumSession('hide');
   session.start(URL, 'chrome');
 
+  bp.sync({ request: bp.Event("loginAdmin") });
   loginAdmin(session, moodledata);
-  createCourse(session,moodledata);
+
+  bp.sync({ request: bp.Event("createCourse") });
+  createCourse(session, moodledata);
 
   bp.sync({ request: bp.Event("navigateToCourse") });
   navigateToCourseFromHomePage(session);
 
+  bp.sync({ request: bp.Event("enrollStudent") });
   enrollStudent(session);
-  enrollTeacher(session);
-  createForum(session,moodledata);
 
+  bp.sync({ request: bp.Event("enrollTeacher") });
+  enrollTeacher(session);
+
+  bp.sync({ request: bp.Event("createForum") });
+  createForum(session, moodledata);
+
+  bp.sync({ request: bp.Event("logoutAdmin") });
   logout(session);
-  loginTeacher(session,moodledata);
+
+  bp.sync({ request: bp.Event("loginTeacher") });
+  loginTeacher(session, moodledata);
 
   bp.sync({ request: bp.Event("navigateToForum") });
   navigateToForum(session);
@@ -65,13 +88,14 @@ bthread('Hide', function () {
   bp.sync({ request: bp.Event("hideForum") });
   hideForum(session);
 
+  bp.sync({ request: bp.Event("checkForumHiding") });
   if (checkForumHiding(session)) {
     console.log('Forum successfully hidden.');
   } else {
     console.error('Forum could not be hidden.');
   }
 
-  bp.sync({ request: bp.Event("logout") });
+  bp.sync({ request: bp.Event("logoutTeacher") });
   logout(session);
 
   session.stop();
